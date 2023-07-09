58f488283f6a81207d1545be035037cd6953aa1b
Francisco Casado Castro <hola@franciscocasado.cl>
2023-07-09 14:53:57 -0400

    move tests to their own folder

--- js/actors.test.js
+++
@@ -1,9 +0,0 @@
-import { Vec } from "./actors.js";
-
-test("sum two vectors", () => {
-  const a = new Vec(1, 2);
-  const b = new Vec(2, 3);
-  const c = new Vec(3, 5);
-  expect(a.plus(b).x).toBe(c.x);
-  expect(a.plus(b).y).toBe(c.y);
-});
